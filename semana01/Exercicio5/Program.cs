1c9825c4d09d2e77b4278b87428e2b883e49f38c
douglas-byu <dleonciodasilva@byupathway.edu>
2026-09-04 23:46:14 -0300

    Implementa Exercicio5 - Funcoes estaticas em C#

--- semana01/Exercicio5/Program.cs
+++ semana01/Exercicio5/Program.cs
@@ -2,8 +2,43 @@ using System;
 
 class Program
 {
+    static void ExibirBoasVindas()
+    {
+        Console.WriteLine("Bem-vindo ao programa!");
+    }
+
+    static string PerguntarNomeUsuario()
+    {
+        Console.Write("Por favor, insira seu nome: ");
+        string nome = Console.ReadLine();
+        return nome;
+    }
+
+    static int PerguntarNumeroFavorito()
+    {
+        Console.Write("Por favor, insira seu número favorito: ");
+        int numero = int.Parse(Console.ReadLine());
+        return numero;
+    }
+
+    static int ElevarAoQuadrado(int numero)
+    {
+        return numero * numero;
+    }
+
+    static void ExibirResultado(string nomeUsuario, int numeroAoQuadrado)
+    {
+        Console.WriteLine($"{nomeUsuario}, o quadrado do seu número é {numeroAoQuadrado}");
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Olá, Mundo! Este é o Projeto Exercicio5.");
+        ExibirBoasVindas();
+
+        string nome = PerguntarNomeUsuario();
+        int numeroFavorito = PerguntarNumeroFavorito();
+        int quadrado = ElevarAoQuadrado(numeroFavorito);
+
+        ExibirResultado(nome, quadrado);
     }
 }
