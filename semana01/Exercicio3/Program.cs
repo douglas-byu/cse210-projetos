b2b5447fd67fd56951e319f5a9eedb16b8d04abd
douglas-byu <dleonciodasilva@byupathway.edu>
2026-09-04 23:38:53 -0300

    Implementa Exercicio3 - Jogo de adivinhacao com loop e contador de palpites

--- semana01/Exercicio3/Program.cs
+++ semana01/Exercicio3/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Olá, Mundo! Este é o Projeto Exercicio3.");
+        string jogarNovamente = "sim";
+
+        while (jogarNovamente == "sim")
+        {
+            Random geradorAleatorio = new Random();
+            int numeroMagico = geradorAleatorio.Next(1, 101);
+
+            int palpite = 0;
+            int contadorPalpites = 0;
+
+            while (palpite != numeroMagico)
+            {
+                Console.Write("Qual é o seu palpite? ");
+                string entradaUsuario = Console.ReadLine();
+                palpite = int.Parse(entradaUsuario);
+                contadorPalpites++;
+
+                if (palpite < numeroMagico)
+                {
+                    Console.WriteLine("Mais alto");
+                }
+                else if (palpite > numeroMagico)
+                {
+                    Console.WriteLine("Mais baixo");
+                }
+                else
+                {
+                    Console.WriteLine("Você adivinhou!");
+                    Console.WriteLine($"Você precisou de {contadorPalpites} palpites.");
+                }
+            }
+
+            Console.Write("Deseja jogar novamente? (sim/não) ");
+            jogarNovamente = Console.ReadLine();
+        }
+
+        Console.WriteLine("Obrigado por jogar!");
     }
 }
